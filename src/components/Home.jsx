ce83bab099fb19ce5ba7090f18f0833db597699f
SwoftCreation <limsoft0401@gmail.com>
2022-11-25 18:13:14 +0900

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,17 +8,30 @@ export default function Home() {
         <span className="Community">Community</span>
         <span className="Products">Products</span>
         <span className="Overview">Overview</span>
+        <span className="Sign-in">Sign in</span>
       </div>
       <div className="Image">
-        <span className="text1">CAPSTONE NBBANG</span>
-        <span className="text2">
-          Smart Control platform with electrode signal
-        </span>
-        <span className="text3">
-          It's not just muscles that can move We expand the limits of our bodies
-        </span>
-        <span className="experience-now">experience now</span>
+        <video autoPlay loop muted poster="image/background.jpg">
+          <source src="video/video.mp4" type="video/mp4" />
+          Browser not support this video
+        </video>
+        <div className="front-texts">
+          <span className="text1">CAPSTONE NBBANG</span>
+          <br />
+          <span className="text2">
+            Smart Control platform with electrode signal
+          </span>
+          <br />
+          <span className="text3">
+            It's not just muscles that can move
+            <br />
+            We expand the limits of our bodies
+          </span>
+          <br />
+          <span className="experience-now">experience now</span>
+        </div>
       </div>
+      <div className="Footer-Frame"></div>
     </div>
   );
 }
