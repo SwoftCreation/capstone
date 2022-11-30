772ad3dc6f8c8c2d05ec6677a57bc7c5370eed5a
SwoftCreation <limsoft0401@gmail.com>
2022-12-01 04:02:50 +0900

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,31 +2,20 @@ import "../styles/Home.scss";
 
 export default function Home() {
   return (
-    <div className="Home-container">
-      <main className="main">
-        <section className="Video-Section">
-          <video autoPlay loop muted placeinline poster="image/background.jpg">
-            <source src="video/video.mp4" type="video/mp4" />
-            Browser not support this video
-          </video>
-        </section>
-
-        <section className="front-texts">
-          <span className="text1">CAPSTONE NBBANG</span>
-          <br />
-          <span className="text2">
-            Smart Control platform with electrode signal
-          </span>
-          <br />
-          <span className="text3">
-            It's not just muscles that can move
-            <br />
-            We expand the limits of our bodies
-          </span>
-          <br />
-          <span className="experience-now">experience now</span>
-        </section>
-      </main>
+    <div className="front-container">
+      <div className="front-texts">
+        <p className="text1">CAPSTONE NBBANG</p>
+        <p className="text2">Smart Control platform with electrode signal</p>
+        <p className="text3">
+          It's not just muscles that can move
+          <br /> We expand the limits of our bodies
+        </p>
+        <div className="button-frame">
+          <div className="front-button">
+            <p>experience now</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
