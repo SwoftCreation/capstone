2582b82a8fe7c7e94566502d08c332a4c58ae0e7
SwoftCreation <limsoft0401@gmail.com>
2022-12-23 17:07:43 +0900

    commit

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../partComponent/Footer";
 import NaviBar from "../partComponent/NaviBar";
 import { useWindupString } from "windups";
+
 export default function Community() {
   const [text] = useWindupString("Community 기능이 추가될 예정입니다");
+  const [post, setPost] = useState({
+    title: "",
+    content: "",
+  });
+  const onChange = () => {};
+
   return (
     <div id="container">
       <NaviBar id="navibar" />
       <div className="front-container">
         <h1>{text}</h1>
+        <div className="front-wrapper">
+          <form>
+            <label>
+              title
+              <input
+                type="text"
+                value={post.title}
+                onChange={onChange}
+                name="title"
+              />
+            </label>
+            <label>
+              content
+              <textare
+            </label>
+          </form>
+        </div>
       </div>
       <Footer />
     </div>
