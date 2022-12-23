9a0f8702908fee85539e9498eb54b26d3ada200c
SwoftCreation <limsoft0401@gmail.com>
2022-12-23 19:26:20 +0900

    commit

--- src/redux/communitySlice.js
+++ src/redux/communitySlice.js
@@ -15,13 +15,19 @@ const communitySlice = createSlice({
   */
   reducers: {
     create: (state, action) => {
+      console.log(action);
       state.totalPostNum += 1;
+      state.posts = [
+        ...state.posts,
+        { title: action.post.title, content: action.post.content },
+      ];
     },
     update: (state, action) => {},
     delete: (state, action) => {
       // TODO: 추후에 이건 아예 게시글이 없으면 dispatch를 못하게 막아야함.
       if (state.totalPostNum !== 0) {
         state.totalPostNum -= 1;
+        state.posts = [];
       } else {
         console.error("error: There is no post now");
       }
