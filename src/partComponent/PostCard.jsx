67c0570c900c74e0467ca9f10c3bf66d9dda77b2
SwoftCreation <limsoft0401@gmail.com>
2022-12-28 14:55:41 +0900

    COMMIT

--- src/partComponent/PostCard.jsx
+++ src/partComponent/PostCard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { CardContent, CardActions, Typography, Button } from "@mui/material";
+import "./PostCard.scss";
+import Swal from "sweetalert2";
 
 export default function PostCard(props) {
   const state = props.post;
   console.log("***전달받은 props***");
   console.log(JSON.stringify(props));
+
+  const onDetailBtnClicked = () => {};
+  const onDeleteBtnClicked = () => {
+    Swal.fire({
+      title: "삭제하시겠습니까?",
+    });
+  };
   return (
-    <div>
+    <div className="CardContainer">
       {state.map((element, key) => (
         <CardContent>
           <CardContent>
@@ -22,9 +31,17 @@ export default function PostCard(props) {
             </Typography>
             <Typography variant="body2">{element.content}</Typography>
           </CardContent>
-          <CardActions>
-            <Button size="small">글 확인하기</Button>
-          </CardActions>
+          <div className="CardButtons">
+            <CardActions>
+              <Button size="small" onClick={onDetailBtnClicked}>
+                글 확인하기
+              </Button>
+            </CardActions>
+            <CardActions onClick={onDeleteBtnClicked}>
+              <Button size="small">삭제</Button>
+            </CardActions>
+          </div>
+
           <hr />
         </CardContent>
       ))}
