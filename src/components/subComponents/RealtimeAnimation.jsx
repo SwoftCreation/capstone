d42b9e15a0f743e14e74fc7ba31317940866e8a4
SwoftCreation <limsoft0401@gmail.com>
2022-12-28 10:10:36 +0900

    commit

--- src/components/subComponents/RealtimeAnimation.jsx
+++ src/components/subComponents/RealtimeAnimation.jsx
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { db } from "../../firebase/firebase";
 import { doc, onSnapshot } from "firebase/firestore";
 
+/*
+FIXME:
+1. 렌더링이 너무 많이 발생을 해서 성능측정을 해보니 이 컴포넌트에서 
+대량의 재렌더링이 발생하고 있었다
+2. 값이 바뀌지 않는다면 렌더링을 막아야한다
+3. 메모이징을 사용해야한다
+*/
 function RealtimeAnimation() {
+  console.log("RealtimeAnimation : Rendered");
+
   const [moveValue, setMoveValue] = useState();
   const unsub = onSnapshot(doc(db, "experience", "fromPy"), (doc) => {
-    setMoveValue(doc.data());
+    setMoveValue(signal(doc.data().Intension));
   });
+  console.log("unsub" + JSON.stringify(unsub));
+
+  const signal = (signal) => {
+    if (signal === 0) return "move right";
+    else if (signal === 1) return "move left";
+    else return "other signal";
+  };
+  /*
+  
+  */
+  console.log("moveValue:" + moveValue, typeof moveValue);
   return (
     <div>
-      <h1>{JSON.stringify(moveValue)}</h1>
+      <h2>{moveValue}</h2>
     </div>
   );
 }
